ae2a3e6353c99b078084032709ad8179519ec73e
artrugh <arturo.rugh@gmail.com>
2020-09-24 15:01:23 +0200

    feat/icon-play

--- src/components/atoms/Image/Image.tsx
+++ src/components/atoms/Image/Image.tsx
@@ -93,7 +93,7 @@ class Image extends Component<IProps> {
 
     return (
       <div
-        className={cn("img-container", classNameContainer, {
+        className={cn("img-container relative mt-48", classNameContainer, {
           "has-gradient": gradientBg,
           "has-animation-hover": animationHover,
         })}
